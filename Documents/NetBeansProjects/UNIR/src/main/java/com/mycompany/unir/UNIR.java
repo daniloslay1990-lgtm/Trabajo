6813596e783e767cf988e3db87c9219252736eab
daniloslay1990-lgtm <daniloslay1990@gmail.com>
2025-08-19 10:48:31 -0500

    Publicación

---
+++ Documents/NetBeansProjects/UNIR/src/main/java/com/mycompany/unir/UNIR.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unir;
+
+/**
+ *
+ * @author USER
+ */
+public class UNIR {
+
+    public static void main(String[] args) {
+        
+        for (int i=1;i<=20;i++) {
+            if(i%2==0) {
+          
+                System.out.println(i);
+                
+            }
+        }
+       
+    }
+}
